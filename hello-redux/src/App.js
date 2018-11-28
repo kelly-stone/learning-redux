594da4bae8764f91f87db7d3f1e49c61446ddb5c
Kelly <kellyandstone@gmail.com>
2018-11-29 05:36:30 +1300

    simply increment and decrement countnumber done

---
+++ hello-redux/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { increment, decrement } from './action'
+
+// import { bindActionCreators } from 'redux'
+
+class App extends Component {
+  render() {
+    // console.log("this.props",this.props)
+    // const { dispatch } = this.props; // means const dispatch = this.props.dispatch
+    console.log("this.props",this.props)
+    const { increment, decrement } = this.props
+    return (
+      <div className="container">
+        <h1 className="jumbotron-heading text-center">{this.props.counter}</h1>
+        <p className="text-center">
+          <button onClick={ () => {increment()}} className="btn btn-primary mr-2">Increase</button> 
+          <button onClick={ () => {decrement()}} className="btn btn-danger my-2">Decrease</button>
+        </p>   
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = (state) =>{ //因为传递述职都是用props
+  // console.log("state", state)
+  return {
+    counter: state.counter //counter is a key
+  }
+}
+
+// const mapDispatchToProps = (dispatch) =>{
+//   // return {
+//   //   increment: bindActionCreators(increment, dispatch)
+//   // }
+//   return bindActionCreators({ increment }, dispatch)
+// }
+
+
+App.propTypes = {
+  counter: PropTypes.number.isRequired,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired
+
+}
+export default connect(mapStateToProps, { increment, decrement })(App);
+
---
+++ hello-redux/src/reducers/user.js
@@ -0,0 +1,9 @@
+const user = (state = "kelly", action) => {
+    switch (action.type) {
+    
+        default:
+            return state;
+    }
+};
+
+export default user
