1b6682cce1ae9f6e04d4dd6eef5e29c0b2179ddf
Kelly <kellyandstone@gmail.com>
2018-12-01 23:59:19 +1300

    add Loading...

--- hello-redux/src/action/index.js
+++ hello-redux/src/action/index.js
@@ -1,6 +1,6 @@
 import axios from'axios'
 import { INCREMENT, DECREMENT} from'../constants'
-import { FETCH_USER_SUCCESS } from'../constants'
+import { FETCH_USER_SUCCESS, FETCH_USER_REQUEST, FETCH_USER_FAILURE } from'../constants'
 
 export const increment = () =>{
     return dispatch =>{
@@ -20,19 +20,38 @@ export const decrement = () =>{
 
 export const get_user = () => {
     return dispatch => {
+        dispatch(fetch_user_request())
         axios.get('https://randomuser.me/api/')
         .then(res => {
+            // console.log(res)
             dispatch(fetch_user(res.data.results[0]))
         })
         .catch(error => {
-            console.log(error)
+            // console.dir(error)
+            dispatch(fetch_user_failure(error.response.data))
         })
     }
 }
+
+export const fetch_user_failure = (error) =>{
+    return{
+        type:FETCH_USER_FAILURE,
+        error
+    }
+}
+
+
+
 
 export const fetch_user = (user) =>{
     return {
         type: FETCH_USER_SUCCESS,
         user
+    }
+}
+
+export const fetch_user_request = () =>{
+    return {
+        type: FETCH_USER_REQUEST,       
     }
 }
--- hello-redux/src/components/User.js
+++ hello-redux/src/components/User.js
@@ -5,12 +5,23 @@ import { get_user } from '../action'
 
 class User extends Component {
   render() {
-    const { get_user,user } = this.props //get_user = this.props.get_user
+    const { get_user } = this.props //get_user = this.props.get_user
+
+    const{ error, isFetching, user } = this.props.user
+    let data;
+
+    if (error) {
+      data=error;
+    }else if(isFetching){
+      data="Loading ...";
+    }else{
+      data = user.email;
+    }
     return (
       <div>
-        <h1 className="jumbotron-heading text-center">{user.email}</h1>
+        <h1 className="jumbotron-heading text-center">{ data }</h1>
         <p className="text-center">
-          <button onClick={() => get_user()} className="btn btn-primary mr-2">GET RANDOM USER</button> 
+          <button onClick={() => get_user()} className="btn btn-success mr-2">GET RANDOM USER</button> 
         </p>   
       </div>
     )
